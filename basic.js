782e8ba329bc134cc1aa0fc2f82bac81d7bee2a4
Nilesh-Patil0310 <146980321+Nilesh-Patil0310@users.noreply.github.com>
2023-10-04 22:26:48 +0530

    add the fundamentals of JS
    Practice files of Javascript

---
+++ basic.js
@@ -0,0 +1,70 @@
+// /*console.log("Js Fundamental !!")
+
+// let age = 10;
+
+// console.log(age)
+
+// age = "ten";
+
+// console.log(age)
+
+// // let, const and var variable declaration and rules for them.
+
+// let value;
+
+// value = 30;
+
+// const pi = 3.14
+
+// var Myname = "Nilesh";
+ 
+// Myname = "Patil";
+
+// */
+
+// /* ************************* *
+// ***  Premetive Data Type   ***
+// **************************** */
+
+// // Number (64 bit)
+// let num = 10;
+// console.log(typeof num);
+
+// // String
+// const firstName = "Nilesh";
+// const lastname = 'Patil';
+// const nickName = `Nilu`;
+
+// console.log(typeof firstName);
+// console.log(typeof lastname);
+// console.log(typeof nickName);
+
+// // Undifined
+// let a;
+// console.log(typeof a);
+// console.log(a);
+
+// // Null 
+// let b = null;
+// console.log(b);
+// console.log(typeof b);
+
+// // Symbol(ES6/2015)
+
+
+// // Bigint(ES2020)
+// let num1 = 10n;
+// console.log(typeof num1)
+
+// // Intresting things
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_VALUE * 10);
+
+// let num3 = Infinity;
+
+// console.log(10/0);
+// console.log(5/Infinity);
+// console.log(10/num3);
+// console.log(num3/num3);
